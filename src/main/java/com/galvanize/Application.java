1d763a872fad471c8fc91fc1140337c1d8484cc4
Amalesan <amalesh_gs@yahoo.co.in>
2020-12-13 17:06:17 -0500

    initial commit

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -3,6 +3,19 @@ package com.galvanize;
 public class Application {
 
     public static void main(String args[]){
-        System.out.print("Hello World");
+        if(null == args){
+            System.out.print("Please specify a name and email");
+        }else{
+            if(args[0].contains("\"")){
+                String output = args[0].replace("\" "," <").replace("\"","").replace(".com",".com>");
+                System.out.print(output);
+            }else if(!args[0].contains(".com")) {
+                System.out.print("Please specify an email for "+args[0]);
+            }
+            else{
+                String output = args[0].replace(" "," <").replace(".com",".com>");
+                System.out.print(output);
+            }
+        }
     }
 }
